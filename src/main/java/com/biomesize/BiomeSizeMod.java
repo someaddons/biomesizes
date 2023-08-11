7371320593a04d43397761a37b013282ae470326
someaddons <someaddons@github.com>
2023-08-11 15:04:00 +0200

    1.20.1 forge

--- src/main/java/com/biomesize/BiomeSizeMod.java
+++ src/main/java/com/biomesize/BiomeSizeMod.java
@@ -1,28 +1,30 @@
 package com.biomesize;
 
-import com.cupboard.config.CupboardConfig;
 import com.biomesize.config.CommonConfiguration;
-import net.fabricmc.api.ModInitializer;
+import com.cupboard.config.CupboardConfig;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class BiomeSizeMod implements ModInitializer
+import static com.biomesize.BiomeSizeMod.MODID;
+
+@Mod(MODID)
+public class BiomeSizeMod
 {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LogManager.getLogger("biomesize");
-    public static final String MODID = "biomesize";
+    public static final String MODID  = "biomesize";
 
-    public static Map<ResourceLocation,Integer> adapted = new HashMap<>();
-    public static CupboardConfig<CommonConfiguration> config = new CupboardConfig<>(MODID,new CommonConfiguration());
+    public static Map<ResourceLocation, Integer>      adapted = new HashMap<>();
+    public static CupboardConfig<CommonConfiguration> config  = new CupboardConfig<>(MODID, new CommonConfiguration());
 
-    @Override
-    public void onInitialize()
+    public BiomeSizeMod()
     {
         adapted.put(new ResourceLocation("minecraft:worldgen/noise/temperature.json"), 0);
         adapted.put(new ResourceLocation("minecraft:worldgen/noise/vegetation.json"), 0);

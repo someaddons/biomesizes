bab7bd87e61e12a4ed49efc487ff38e349f69a58
someaddons <someaddons@github.com>
2026-01-28 15:50:23 +0100

    1.6: Reworked functionality, allows better configuration and per dimension adjustments

--- src/main/java/com/biomesize/BiomeSizeMod.java
+++ src/main/java/com/biomesize/BiomeSizeMod.java
@@ -3,6 +3,8 @@ package com.biomesize;
 import com.cupboard.config.CupboardConfig;
 import com.biomesize.config.CommonConfiguration;
 import net.fabricmc.api.ModInitializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,6 +19,12 @@ public class BiomeSizeMod implements ModInitializer
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LogManager.getLogger("biomesize");
     public static final String MODID = "biomesize";
+    public static final String NOISE_ROUTER    = "noise_router";
+    public static final String TEMPERATURE     = "temperature";
+    public static final String VEGETATION      = "vegetation";
+    public static final String XZ_SCALE        = "xz_scale";
+    public static final String NOISE           = "noise";
+    public static final String SIZE_HORIZONTAL = "size_horizontal";
 
     public static Map<ResourceLocation,Integer> adapted = new HashMap<>();
     public static CupboardConfig<CommonConfiguration> config = new CupboardConfig<>(MODID,new CommonConfiguration());
@@ -26,5 +34,61 @@ public class BiomeSizeMod implements ModInitializer
     {
         adapted.put(new ResourceLocation("minecraft:worldgen/noise/temperature.json"), 0);
         adapted.put(new ResourceLocation("minecraft:worldgen/noise/vegetation.json"), 0);
+    }
+
+    public static void adjustJsonData(final JsonElement jsonElement, final ResourceLocation id)
+    {
+        if (jsonElement instanceof JsonObject fileJson)
+        {
+            final String configID = id.getNamespace() + ":" + id.getPath().replace("worldgen/noise_settings/", "").replace(".json", "");
+
+            if (fileJson.has(NOISE_ROUTER))
+            {
+                final JsonObject noiseJson = fileJson.get(NOISE_ROUTER).getAsJsonObject();
+                if (noiseJson.has(TEMPERATURE) && noiseJson.has(VEGETATION))
+                {
+                    if (noiseJson.get(TEMPERATURE).isJsonObject() && noiseJson.get(VEGETATION).isJsonObject())
+                    {
+                        // Overworld style worldgen
+                        final JsonObject temperatureJson = noiseJson.getAsJsonObject(TEMPERATURE).getAsJsonObject();
+                        final JsonObject vegetationJson = noiseJson.getAsJsonObject(VEGETATION).getAsJsonObject();
+
+                        if (config.getCommonConfig().createSettingsFor(configID, false))
+                        {
+                            return;
+                        }
+
+                        double scale = config.getCommonConfig().getScale(configID);
+
+                        if (temperatureJson.has(XZ_SCALE))
+                        {
+                            temperatureJson.addProperty(XZ_SCALE, temperatureJson.get(XZ_SCALE).getAsDouble() * 1 / scale);
+                        }
+                        if (vegetationJson.has(XZ_SCALE))
+                        {
+                            vegetationJson.addProperty(XZ_SCALE, vegetationJson.get(XZ_SCALE).getAsDouble() * 1 / scale);
+                        }
+
+                        return;
+                    }
+                }
+            }
+
+            if (fileJson.has(NOISE) && fileJson.get(NOISE).isJsonObject())
+            {
+                final JsonObject noiseJson = fileJson.get(NOISE).getAsJsonObject();
+                if (noiseJson.has(SIZE_HORIZONTAL))
+                {
+                    if (config.getCommonConfig().createSettingsFor(configID, true))
+                    {
+                        return;
+                    }
+
+                    int scale = (int) config.getCommonConfig().getScale(configID);
+
+                    noiseJson.addProperty(SIZE_HORIZONTAL, noiseJson.get(SIZE_HORIZONTAL).getAsInt() + scale);
+                }
+            }
+        }
     }
 }
--- src/main/java/com/biomesize/config/CommonConfiguration.java
+++ src/main/java/com/biomesize/config/CommonConfiguration.java
@@ -1,27 +1,115 @@
 package com.biomesize.config;
 
+import com.biomesize.BiomeSizeMod;
 import com.cupboard.config.ICommonConfig;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import it.unimi.dsi.fastutil.objects.*;
 
-public class CommonConfiguration implements ICommonConfig {
+import java.util.Map;
 
-    public int biomeSizeModifier = -2;
+public class CommonConfiguration implements ICommonConfig
+{
 
-    public CommonConfiguration() {
+    public  int                              biomeSizeModifier   = -2;
+    public  boolean                                legacyMode          = false;
+    private Object2DoubleLinkedOpenHashMap<String> scaleSettings       = new Object2DoubleLinkedOpenHashMap<>();
+    private Object2IntLinkedOpenHashMap<String>          simpleScaleSettings = new Object2IntLinkedOpenHashMap<>();
+
+    public CommonConfiguration()
+    {
     }
 
-    public JsonObject serialize() {
+    public JsonObject serialize()
+    {
         final JsonObject root = new JsonObject();
 
         final JsonObject entry = new JsonObject();
-        entry.addProperty("desc:", "Biome size modifier, added to the vanilla biome size. Vanilla value:0, min = -8 max = 8 : default:-2");
-        entry.addProperty("biomeSizeModifier", biomeSizeModifier);
-        root.add("biomeSizeModifier", entry);
+        entry.addProperty("desc:", "Biome size settings per noise setting, set by default to no changes. Gets automatically filled on world creation. Restart after changing values."
+            + "Note that the biome size changes work indirectly by modifying temperature and vegetation zone sizes and you cannot change the size of a specific biome");
+
+        for (final Object2DoubleMap.Entry<String> scaleEntry : scaleSettings.object2DoubleEntrySet())
+        {
+            final JsonObject scaleEntryJson = new JsonObject();
+            scaleEntryJson.addProperty("desc", "Set the multiplicative modifier for biome size. 1.0 = Default size, 2.0 = bigger, 0.5 = smaller.");
+            scaleEntryJson.addProperty("size_multiplier", scaleEntry.getDoubleValue());
+            entry.add(scaleEntry.getKey(), scaleEntryJson);
+        }
+
+        for (final Object2IntMap.Entry<String> scaleEntry : simpleScaleSettings.object2IntEntrySet())
+        {
+            final JsonObject scaleEntryJson = new JsonObject();
+            scaleEntryJson.addProperty("desc",
+                "Set the horizontal size offset, this dimension only has limited size changing support. You can only step sizes in whole numbers, not percentages. 0 = Default size, 1 = bigger, -1 = smaller. Range[-1,2]");
+            scaleEntryJson.addProperty("horizontal_size", scaleEntry.getIntValue());
+            entry.add(scaleEntry.getKey(), scaleEntryJson);
+        }
+
+        root.add("worldSettings", entry);
 
         return root;
     }
 
-    public void deserialize(JsonObject data) {
-        biomeSizeModifier = data.get("biomeSizeModifier").getAsJsonObject().get("biomeSizeModifier").getAsInt();
+    public void deserialize(JsonObject data)
+    {
+        if (data.has("biomeSizeModifier"))
+        {
+            legacyMode = true;
+            biomeSizeModifier = data.get("biomeSizeModifier").getAsJsonObject().get("biomeSizeModifier").getAsInt();
+        }
+        else
+        {
+            final JsonObject dimensionJson = data.get("worldSettings").getAsJsonObject();
+            for (final Map.Entry<String, JsonElement> entry : dimensionJson.entrySet())
+            {
+                if (entry.getValue().isJsonObject())
+                {
+                    final JsonObject entryJson = entry.getValue().getAsJsonObject();
+                    if (entryJson.has("size_multiplier"))
+                    {
+                        scaleSettings.put(entry.getKey(), entryJson.get("size_multiplier").getAsDouble());
+                    }
+                    else
+                    {
+                        simpleScaleSettings.put(entry.getKey(), entryJson.get("horizontal_size").getAsInt());
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean createSettingsFor(final String configID, final boolean simpleIntValue)
+    {
+        if (scaleSettings.containsKey(configID) || simpleScaleSettings.containsKey(configID))
+        {
+            return false;
+        }
+
+        if (simpleIntValue)
+        {
+            simpleScaleSettings.put(configID, 0);
+        }
+        else
+        {
+            scaleSettings.put(configID, 1.0);
+        }
+
+        BiomeSizeMod.config.save();
+        return true;
+    }
+
+    public double getScale(final String configID)
+    {
+        if (scaleSettings.containsKey(configID))
+        {
+            return scaleSettings.getDouble(configID);
+        }
+
+        if (simpleScaleSettings.containsKey(configID))
+        {
+            return simpleScaleSettings.getInt(configID);
+        }
+
+        return 0;
     }
 }

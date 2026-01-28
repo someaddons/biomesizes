bf1021bfd28f9a1457e9cfac4c6c4a3ea521e40b
someaddons <someaddons@github.com>
2026-01-28 15:50:23 +0100

    1.6: Reworked functionality, allows better configuration and per dimension adjustments

--- src/main/java/com/biomesize/BiomeSizeMod.java
+++ src/main/java/com/biomesize/BiomeSizeMod.java
@@ -2,6 +2,8 @@ package com.biomesize;
 
 import com.biomesize.config.CommonConfiguration;
 import com.cupboard.config.CupboardConfig;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
@@ -18,8 +20,14 @@ public class BiomeSizeMod
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LogManager.getLogger("biomesize");
-    public static final String MODID  = "biomesize";
+    public static final Logger LOGGER          = LogManager.getLogger("biomesize");
+    public static final String MODID           = "biomesize";
+    public static final String NOISE_ROUTER    = "noise_router";
+    public static final String TEMPERATURE     = "temperature";
+    public static final String VEGETATION      = "vegetation";
+    public static final String XZ_SCALE        = "xz_scale";
+    public static final String NOISE           = "noise";
+    public static final String SIZE_HORIZONTAL = "size_horizontal";
 
     public static Map<ResourceLocation, Integer>      adapted = new HashMap<>();
     public static CupboardConfig<CommonConfiguration> config  = new CupboardConfig<>(MODID, new CommonConfiguration());
@@ -28,5 +36,61 @@ public class BiomeSizeMod
     {
         adapted.put(new ResourceLocation("minecraft:worldgen/noise/temperature.json"), 0);
         adapted.put(new ResourceLocation("minecraft:worldgen/noise/vegetation.json"), 0);
+    }
+
+    public static void adjustJsonData(final JsonElement jsonElement, final ResourceLocation id)
+    {
+        if (jsonElement instanceof JsonObject fileJson)
+        {
+            final String configID = id.getNamespace() + ":" + id.getPath().replace("worldgen/noise_settings/", "").replace(".json", "");
+
+            if (fileJson.has(NOISE_ROUTER))
+            {
+                final JsonObject noiseJson = fileJson.get(NOISE_ROUTER).getAsJsonObject();
+                if (noiseJson.has(TEMPERATURE) && noiseJson.has(VEGETATION))
+                {
+                    if (noiseJson.get(TEMPERATURE).isJsonObject() && noiseJson.get(VEGETATION).isJsonObject())
+                    {
+                        // Overworld style worldgen
+                        final JsonObject temperatureJson = noiseJson.getAsJsonObject(TEMPERATURE).getAsJsonObject();
+                        final JsonObject vegetationJson = noiseJson.getAsJsonObject(VEGETATION).getAsJsonObject();
+
+                        if (config.getCommonConfig().createSettingsFor(configID, false))
+                        {
+                            return;
+                        }
+
+                        double scale = config.getCommonConfig().getScale(configID);
+
+                        if (temperatureJson.has(XZ_SCALE))
+                        {
+                            temperatureJson.addProperty(XZ_SCALE, temperatureJson.get(XZ_SCALE).getAsDouble() * 1 / scale);
+                        }
+                        if (vegetationJson.has(XZ_SCALE))
+                        {
+                            vegetationJson.addProperty(XZ_SCALE, vegetationJson.get(XZ_SCALE).getAsDouble() * 1 / scale);
+                        }
+
+                        return;
+                    }
+                }
+            }
+
+            if (fileJson.has(NOISE) && fileJson.get(NOISE).isJsonObject())
+            {
+                final JsonObject noiseJson = fileJson.get(NOISE).getAsJsonObject();
+                if (noiseJson.has(SIZE_HORIZONTAL))
+                {
+                    if (config.getCommonConfig().createSettingsFor(configID, true))
+                    {
+                        return;
+                    }
+
+                    int scale = (int) config.getCommonConfig().getScale(configID);
+
+                    noiseJson.addProperty(SIZE_HORIZONTAL, noiseJson.get(SIZE_HORIZONTAL).getAsInt() + scale);
+                }
+            }
+        }
     }
 }
--- src/main/java/com/biomesize/mixin/RegistryDataLoaderMixin.java
+++ src/main/java/com/biomesize/mixin/RegistryDataLoaderMixin.java
@@ -1,9 +1,9 @@
 package com.biomesize.mixin;
 
+import com.biomesize.BiomeSizeMod;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.mojang.serialization.Decoder;
-import com.biomesize.BiomeSizeMod;
 import net.minecraft.core.Registry;
 import net.minecraft.core.WritableRegistry;
 import net.minecraft.resources.*;
@@ -19,34 +19,54 @@ import java.io.Reader;
 import java.util.Iterator;
 import java.util.Map;
 
+import static com.biomesize.BiomeSizeMod.adjustJsonData;
+
 @Mixin(RegistryDataLoader.class)
 public class RegistryDataLoaderMixin
 {
     @Inject(method = "loadRegistryContents", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Decoder;parse(Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;", remap = false), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-    private  static <E> void onLoad(
-      final RegistryOps.RegistryInfoLookup registryInfoLookup,
-      final ResourceManager resourceManager,
-      final ResourceKey<? extends Registry<E>> resourceKey,
-      final WritableRegistry<E> writableRegistry,
-      final Decoder<E> decoder,
-      final Map<ResourceKey<?>, Exception> map,
-      final CallbackInfo ci,
-      final String string,
-      final FileToIdConverter fileToIdConverter,
-      final RegistryOps registryOps,
-      final Iterator var9,
-      final Map.Entry entry,
-      final ResourceLocation resourceLocation,
-      final ResourceKey resourceKey2,
-      final Resource resource,
-      final Reader reader,
-      final JsonElement jsonElement)
+    private static <E> void onLoad(
+        final RegistryOps.RegistryInfoLookup registryInfoLookup,
+        final ResourceManager resourceManager,
+        final ResourceKey<? extends Registry<E>> resourceKey,
+        final WritableRegistry<E> writableRegistry,
+        final Decoder<E> decoder,
+        final Map<ResourceKey<?>, Exception> map,
+        final CallbackInfo ci,
+        final String string,
+        final FileToIdConverter fileToIdConverter,
+        final RegistryOps registryOps,
+        final Iterator var9,
+        final Map.Entry entry,
+        final ResourceLocation resourceLocation,
+        final ResourceKey resourceKey2,
+        final Resource resource,
+        final Reader reader,
+        final JsonElement jsonElement)
     {
-        if (BiomeSizeMod.adapted.containsKey(resourceLocation))
+        if (BiomeSizeMod.config.getCommonConfig().legacyMode)
         {
-            if (jsonElement instanceof JsonObject)
+            if (BiomeSizeMod.adapted.containsKey(resourceLocation))
             {
-                ((JsonObject) jsonElement).addProperty("firstOctave", ((JsonObject) jsonElement).get("firstOctave").getAsInt() - BiomeSizeMod.config.getCommonConfig().biomeSizeModifier);
+                if (jsonElement instanceof JsonObject)
+                {
+                    ((JsonObject) jsonElement).addProperty("firstOctave",
+                        ((JsonObject) jsonElement).get("firstOctave").getAsInt() - BiomeSizeMod.config.getCommonConfig().biomeSizeModifier);
+                }
+            }
+
+            return;
+        }
+
+        if (resourceKey.location().getPath().contains("worldgen/noise_settings"))
+        {
+            try
+            {
+                adjustJsonData(jsonElement, resourceLocation);
+            }
+            catch (Exception e)
+            {
+                BiomeSizeMod.LOGGER.error("Failed to adjust:"+resourceLocation+" data:"+jsonElement, e);
             }
         }
     }
